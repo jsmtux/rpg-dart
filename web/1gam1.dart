8fb421e30322f0e11ac3c699014bcdd76fd5a4f5
jsmtux <jsmtux@gmail.com>
2015-03-19 10:26:35 +0000

    Simplified level loading

--- web/1gam1.dart
+++ web/1gam1.dart
@@ -1,30 +1,12 @@
 import 'dart:html';
-import 'dart:async';
 
 import 'package:game_loop/game_loop_html.dart';
 
 import 'renderer.dart';
 import 'game_state.dart';
 import 'drawable_factory.dart';
-import 'level_importer.dart';
-import 'level_data.dart';
-import 'behaviour.dart';
 import 'camera.dart';
-import 'sprite_importer.dart';
 
-void initGame (TerrainBehaviour terrain, GameState state, DrawableFactory drawable_factory, GameLoopHtml gameLoop, Camera cur_cam)
-{
-  SpriteImporter sprite_importer = new SpriteImporter();
-  sprite_importer.RequestFile("images/map_units_test.json").then(
-      (loader)
-      {
-        loader.configure(terrain, state, drawable_factory, gameLoop, cur_cam);
-        loader.AddToGameState();
-      }
-  );
-
-  gameLoop.start();
-}
 
 main() {
   CanvasElement canvas = querySelector(".game-element");
@@ -34,13 +16,10 @@ main() {
   DrawableFactory drawable_factory = new DrawableFactory(renderer);
   GameState draw_state = new GameState(renderer);
 
-  LevelImporter level_importer = new LevelImporter();
-  Future<TerrainBehaviour> import_res =
-      level_importer.RequestFile("images/map_test.json").then((LevelData data) => (data.AddToGameState(draw_state, drawable_factory)));
-
   Camera cur_cam = new Camera(renderer.m_worldview_);
 
-  gameLoop.state = draw_state;
+  draw_state.loadTerrain("images/map_test.json", drawable_factory)
+    .then((res) => draw_state.initBehaviour("images/map_units_test.json",res, draw_state, drawable_factory, gameLoop, cur_cam));
 
-  import_res.then((res) => initGame(res, draw_state, drawable_factory, gameLoop, cur_cam));
+  gameLoop.start();
 }
--- web/game_state.dart
+++ web/game_state.dart
@@ -1,6 +1,7 @@
 library game_state;
 
 import 'dart:html';
+import 'dart:async';
 
 import 'package:game_loop/game_loop_html.dart';
 
@@ -8,6 +9,11 @@ import 'drawable.dart';
 import 'renderer.dart';
 import 'behaviour.dart';
 import 'path.dart';
+import 'level_importer.dart';
+import 'level_data.dart';
+import 'drawable_factory.dart';
+import 'sprite_importer.dart';
+import 'camera.dart';
 
 class GameState extends SimpleHtmlState
 {
@@ -15,6 +21,27 @@ class GameState extends SimpleHtmlState
   List<Drawable> drawables_ = new List<Drawable>();
   List<Behaviour> behaviours_ = new List<Behaviour>();
   Map<String, Path> paths_ = new Map<String, Path>();
+
+  Future<TerrainBehaviour> loadTerrain(String level_path, DrawableFactory drawable_factory)
+  {
+    LevelImporter level_importer = new LevelImporter();
+    return level_importer.RequestFile(level_path).then((LevelData data) => (data.AddToGameState(this, drawable_factory)));
+  }
+
+  void initBehaviour(String behaviour_path, TerrainBehaviour terrain, GameState state, DrawableFactory drawable_factory, GameLoopHtml gameLoop, Camera cur_cam)
+  {
+    SpriteImporter sprite_importer = new SpriteImporter();
+    sprite_importer.RequestFile(behaviour_path).then(
+      (loader)
+      {
+        loader.configure(terrain, state, drawable_factory, gameLoop, cur_cam);
+        loader.AddToGameState();
+      }
+    );
+
+    gameLoop.state = this;
+
+  }
 
   void addElement(Drawable drawable, Behaviour behaviour)
   {
