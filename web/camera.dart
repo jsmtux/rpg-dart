c19c37d3de1e6ef8c860a6a5f451a3fd7c4eb983
jsmtux <jsmtux@gmail.com>
2015-04-13 22:14:55 +0100

    Improved sign

--- web/camera.dart
+++ web/camera.dart
@@ -9,13 +9,13 @@ class Camera
   Camera(this.cam_mat)
   {
     cam_mat.rotate(new Vector3(-1.0,0.0,0.0), radians(45.0));
-    cam_mat.translate(-5.0, 3.0, -15.0);
+    cam_mat.translate(-4.0, 3.0, -10.0);
   }
 
   void SetPos(Vector2 vec)
   {
     cam_mat.setIdentity();
     cam_mat.rotate(new Vector3(-1.0,0.0,0.0), radians(45.0));
-    cam_mat.translate(3.0 + vec.x, 10.0 + vec.y, -15.0);
+    cam_mat.translate(3.0 + vec.x, 10.0 + vec.y, -10.0);
   }
 }
